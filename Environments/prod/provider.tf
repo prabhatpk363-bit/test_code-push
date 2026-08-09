ae0aa0a8d24279459a58f6e55d4a7c3daba637be
prabhatpk363-bit <prabhatpk363@gmail.com>
2026-08-09 13:55:59 +0530

    backend corrected

--- Environments/prod/provider.tf
+++ Environments/prod/provider.tf
@@ -8,8 +8,8 @@ terraform {
     backend "azurerm" {
     resource_group_name  = "prod-pk"
     storage_account_name = "pkstorage1231"
-    container_name       = "pkcontainer"
-    key                  = "prabhat.tfstate"
+    container_name       = "pkcontainer1"
+    key                  = "pk.tfstate"
 
   }
 }
